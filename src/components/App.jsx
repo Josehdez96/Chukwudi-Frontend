5f0965b388fa19735298ae5bb9bf15b81a46f514
Jose Daniel Hernandez <josedanielhq@gmail.com>
2020-07-31 15:35:44 -0500

    Listo ambiente, comenzamos a maquetar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './images/logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <main id="app">Main view</main>
+      <div>Menu</div>
+      <div>SearchBar</div>
+      <div>main publicity</div>
+      <div>title Restaurants</div>
+      <div>Order time</div>
+      <div>Carrousel with kind of foods</div>
+      <div>Carrousel with specific foods</div>
+
+      <section>My order</section>
+      <div>
+        <div>User</div>
+        <div>Count of items in my order</div>
+      </div>
+
+      <div>Title my order</div>
+      <div>Info address and time</div>
+      <div>List of foods in My Order</div>
+      <div>Tips for delivery</div>
+      <div>
+        <div>Total title</div>
+        <div>Total money amount</div>
+      </div>
+      <div>Persons?</div>
+      <div>Checkout button</div>
+    </Fragment>
   );
 }
 
